2834e7273a028d262bcd1111ec703417433afdb3
Kevin Simon <keviniansimon@gmail.com>
2024-10-24 16:38:56 +0200

    build: setup project structure

---
+++ src/app/components/ui/Button.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+interface ButtonProps {
+  children: string;
+}
+
+const Button: FC<ButtonProps> = ({}) => {
+  return <div>Button</div>;
+};
+
+export default Button;
